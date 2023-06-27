63c3b5b0caedaa3ffa6a2e8979b562053996c80a
Kamran <311.kamran@gmail.com>
2023-06-27 04:46:06 +0400

    Complete ECommerce.Core

---
+++ ECommerce.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using ECommerce.Core.Abstraction;
+
+
+namespace ECommerce.Core.DataAccess;
+
+public interface IEntityRepository<T> where T : class, IEntity, new()
+{
+	T Get(Expression<Func<T, bool>> filter = null);
+	List<T> GetList(Expression<Func<T, bool>> filter = null);
+	void Add(T entity);
+	void Update(T entity);
+	void Delete(T entity);
+}
